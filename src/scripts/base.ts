32a634aaa4fb7d84ca3c4991cc880f8b30346fdb
Kyoto Kopz <kopz9999@gmail.com>
2017-08-20 04:59:19 -0700

    Oauth Workflow complete

--- src/scripts/base.ts
+++ src/scripts/base.ts
@@ -4,13 +4,16 @@ import {Promise} from "core-js";
 import popsicle = require("popsicle/dist/common");
 import {Request, RequestOptions} from "popsicle/dist/request";
 import { Response } from "popsicle/dist/response";
+import {error} from "util";
 
 const AUTH_URL = "https://drchrono.com/o/authorize/";
 const TOKEN_URL = "https://drchrono.com/o/token/";
 
+const ACCESS_TOKEN_NAME = "accessToken";
+
 export class Base {
   public auth: ClientOAuth2;
-  public currentToken: ClientOAuth2.Token;
+  public currentToken: ClientOAuth2.Token | null;
   public clientId: string;
   public clientSecret: string;
   public redirectUrl: string;
@@ -35,6 +38,9 @@ export class Base {
   }
 
   public callResource(resource: string, method: "GET", body: {}): Request {
+    if (!this.currentToken) {
+      throw new Error("No Access Token");
+    }
     return popsicle.request({
       body: {
         access_token: this.currentToken.accessToken,
@@ -47,23 +53,56 @@ export class Base {
     });
   }
 
-  public getToken(): Promise<ClientOAuth2.Token> {
+  public get currentCode():string {
     const url = new URL(window.location.href);
     const searchParams = new URLSearchParams(url.search);
-    const code = searchParams.get("code");
+    return searchParams.get("code") || "";
+  }
 
+  public getToken(): Promise<ClientOAuth2.Token | null> {
     return popsicle.post({
       body: {
         client_id: this.clientId,
         client_secret: this.clientSecret,
-        code,
+        code: this.currentCode,
         grant_type: "authorization_code",
         redirect_uri: this.redirectUrl,
       },
       url: `${this.proxyApiUrl}createToken`,
     }).then((response: Response) => {
-      this.currentToken = this.auth.createToken(JSON.parse(response.toJSON().body));
-      return this.currentToken;
+      if (response.status >= 400) {
+        throw response.toJSON().body;
+      } else {
+        return this.auth.createToken(JSON.parse(response.toJSON().body));
+      }
     });
+  }
+
+  public authWorkflow(): Promise<ClientOAuth2.Token> {
+    return new Promise((resolve, reject) => {
+      this.currentToken = this.retrieveAccessToken();
+      if (this.currentToken) {
+        resolve(this.currentToken);
+      } else {
+        if (this.currentCode) {
+          return this.getToken().then((token: ClientOAuth2.Token) => {
+            this.currentToken = token;
+            localStorage.setItem(ACCESS_TOKEN_NAME, JSON.stringify(token.data));
+            return token;
+          }).catch(() => this.requestAccess());
+        } else {
+          this.requestAccess();
+        }
+      }
+    });
+  }
+
+  public retrieveAccessToken(tokenName: string = ACCESS_TOKEN_NAME): ClientOAuth2.Token | null {
+    const rawAccessToken = localStorage.getItem(tokenName);
+    if (rawAccessToken) {
+      return this.auth.createToken(JSON.parse(rawAccessToken));
+    } else {
+      return null;
+    }
   }
 }
